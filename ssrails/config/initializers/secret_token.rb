c9613edec3dfdc5716c2c8634b8ac84f63e18add
murphybytes <murphybytes@gmail.com>
2010-12-04 21:45:36 -0600

    adding application

---
+++ ssrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ssrails::Application.config.secret_token = '396b46927f2da2fbf2bdd3ef68f8ac91a2b95078eaa92df0284bde73ebf268217c4fa02bc7f197f0c2ddbb5c04ebcc4bb8ef766312e701b368c6d6c6cbac7e27'
