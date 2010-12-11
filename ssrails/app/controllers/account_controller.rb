639ca2925f44ef7f6f30b46ef63135b84009e396
murphybytes <murphybytes@gmail.com>
2010-12-11 08:12:23 -0600

    got fb auth working

--- ssrails/app/controllers/account_controller.rb
+++ ssrails/app/controllers/account_controller.rb
@@ -1,5 +1,43 @@
 class AccountController < ApplicationController
+  
   def index
+  end
+
+  def authorize
+    logger.debug "called authorize"
+    redirect_to client.web_server.authorize_url(
+      :redirect_uri => redirect_uri,
+      :scope => 'email,user_photos')
+  end
+  
+  def callback
+    logger.debug "called callback"
+    @access_token = client.web_server.get_access_token( params[:code],    :redirect_uri => redirect_uri )
+    
+    logger.debug "ACCESS TOKEN -> #{ @access_token.inspect }"
+    user = JSON.parse( @access_token.get( '/me'))
+    logger.debug user.inspect
+    redirect_to :action => 'index'
+  end
+
+  private
+  def init
+    logger.debug "called init"
+    @config = YAML::load_file('config/application.yml')[RAILS_ENV]
+
+  end
+
+  def client
+    config = YAML::load_file('config/application.yml')[RAILS_ENV]
+    OAuth2::Client.new( config['app_id'], config['secret'], :site => 'https://graph.facebook.com')
+  end
+
+  def redirect_uri
+    uri = URI.parse(request.url)
+    uri.path = '/account/callback'
+    uri.query = nil
+    logger.debug "redirect uri -> #{uri.to_s}"
+    uri.to_s
   end
 
 end
--- ssrails/app/controllers/application_controller.rb
+++ ssrails/app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def authorize
+    user_state = nil
+    if session.key?(:facebook_session_id)
+      user_state = Session.find( session[:facebook_session_id]  )
+    end
+    # if this is the first time we've seen this user
+    # hang on to the url that we are trying to go to
+    # so we can come back after we log into facebook
+    unless user_state
+      session[:redirect] = request.url
+      redirect_to 'account/authorize'
+    end
+
+    @facebook_access_code = user_state.facebook_code
+  end
+
+
+
 end
